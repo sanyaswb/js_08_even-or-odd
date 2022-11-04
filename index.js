d94075dd18758af9753dd65d44f7a199bdb9b642
sanyaswb <sanyaswb@gmail.com>
2022-11-04 11:19:28 +0200

    completed 08_even-or-odd switch

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function evenOrOdd(n) {
-	// write your code here
+	switch (true) {
+		case n < 0:
+			return 'NEGATIVE';
+		case n === 0:
+			return 'ZERO';
+		case n % 2 === 0:
+			return 'EVEN';
+		case n % 2 === 1:
+			return 'ODD';
+		default:
+			return false;
+	}
 }
 
 module.exports = evenOrOdd;
