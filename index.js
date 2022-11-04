7dab31a07d0b30b9605eb2faf266a7d4b2e07f36
sanyaswb <sanyaswb@gmail.com>
2022-11-04 11:37:59 +0200

    completed 08_even-or-odd ternary

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function evenOrOdd(n) {
-	// write your code here
+	return n < 0 ? 'NEGATIVE' : n === 0 ? 'ZERO' : n % 2 === 0 ? 'EVEN' : n % 2 === 1 ? 'ODD' : false;
 }
 
 module.exports = evenOrOdd;
