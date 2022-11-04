994785d80ac9ac0ec47176257551433520e84973
sanyaswb <sanyaswb@gmail.com>
2022-11-04 11:08:56 +0200

    completed 08_even-or-odd default

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function evenOrOdd(n) {
-	// write your code here
+	if (n < 0) {
+		return 'NEGATIVE';
+	} else if (n === 0) {
+		return 'ZERO';
+	} else if (n % 2 === 0) {
+		return 'EVEN';
+	} else if (n % 2 === 1) {
+		return 'ODD';
+	}
 }
 
 module.exports = evenOrOdd;
